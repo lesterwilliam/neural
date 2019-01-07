68a11d3ffd3dd87d89d41a086f6e54d79840dcf2
Adrian Schwizgebel <adrian.schwizgebel@gmail.com>
2019-01-07 15:47:04 +0100

    Added external activation-function-file

---
+++ nn_activation_function.py
@@ -0,0 +1,16 @@
+# Activation function: Gradient descent
+def av_f(x):
+	return sgmd(x)
+def av_f_d(x):
+	return sgmd_d(x)
+
+# Sigmoidal function
+def sgmd(x):
+	return 1.0/(1+ np.exp(-x))
+def sgmd_d(x):
+	return x * (1.0 - x)
+# Tangens hyperbolicus function
+def tanh(x):
+	return np.tanh(x)
+def tanh_d(x):
+	return 1 - np.tanh(x)
