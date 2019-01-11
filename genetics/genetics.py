c745029d567bb70bbf2e1243b53310da78948ffc
lesterwilliam <adrian.schwizgebel@gmail.com>
2019-01-11 09:02:41 +0100

    Improved genetics

--- genetics/genetics.py
+++ genetics/genetics.py
@@ -1,25 +1,47 @@
-# Thanks to https://blog.sicara.com/getting-started-genetic-algorithms-python-tutorial-81ffa1dd72f9
+# This file contains the basic functions for genetic reproduction
 
+# Import libraries
 import random
 import numpy as np
 
+# Chance to mutate
+mutationChance = 0.1
+# Factor a mutation can have compared to the pure input
+mutationFactor = 2
+
+# Test data:
+#pure = [[0,0,0],
+#		[0,0,0],
+#		[0,0,0]]
+
+#parentA = [[1.0,1.0,1.0,1.0],
+#		[0.0,0.0,0.0,0.0],
+#		[0.0,0.0,0.0,0.0],
+#		[0.0,0.0,0.0,0.0]]
+
+#parentB = [[0.0,0.0,0.0,0.0],
+#		[0.0,0.0,0.0,0.0],
+#		[0.0,0.0,0.0,0.0],
+#		[0.0,0.0,0.0,0.0]]
+
 # Mixes genes from parents and returns created child
 def breedChild(parentA, parentB):
-	if len(parentA) != len(parentB):
-		return 0
-	child = np.zeros((len(parentA),1))
-	for item in range(len(parentA)):
-		if (int(100 * random.random()) < 50):
-			child[item] = parentA[item]
-		else:
-			child[item] = parentB[item]
+	parentA = np.matrix(parentA)
+	parentB = np.matrix(parentB)
+	child = np.full_like(parentA, 0)
+	for i in range(np.shape(child)[0]):
+		for j in range(np.shape(child)[1]):
+			if (int(100 * random.random()) < 50):
+				child[i,j] = parentA[i,j]
+			else:
+				child[i,j] = parentB[i,j]
 	return child
 
-# Creates a random mutation and returns new mutated gene
-def mutate(pure, mutationFactor):
+# Creates a random binary mutation and returns new mutated gene
+def mutate_binary(pure, mutationChance):
 	mutant = np.zeros((len(pure),1))
 	for gene in range(len(pure)):
-		if (100 * random.random() < (mutationFactor * 100)):
+		if (100 * random.random() < (mutationChance * 100)):
 			if pure[gene] == 0:
 				mutant[gene] = 1
 			else:
@@ -27,3 +49,16 @@ def mutate(pure, mutationFactor):
 		else:
 			mutant[gene] = pure[gene]
 	return mutant
+
+# Creates a random floating mutation and returns new mutated gene
+def mutate_floating(pure, mutationChance, mutationFactor):
+	mutant = np.copy(pure)
+	for i in range(np.shape(mutant)[0]):
+		for j in range(np.shape(mutant)[1]):
+			if (100 * random.random() < (mutationChance * 100)):
+				mutant[i,j] = pure[i,j] * (random.uniform(-1.0*(mutationFactor),1.0*(mutationFactor)))
+				if (mutant[i,j] == 0):
+					mutant[i,j] = random.uniform(-1,1)
+	return mutant
+
+#print (mutate_floating(breedChild(parentA, parentB), mutationChance, mutationFactor))
